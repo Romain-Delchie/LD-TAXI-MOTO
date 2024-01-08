84abfcafa122ea6ff1d423bff873425931f46586
Romain-Delchie <roomsrio7782@gmail.com>
2024-01-08 23:14:35 +0100

    add mui

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Reservation.css";
+import AddressInput from "../AddressInput/AddressInput";
 
 export default function Reservation() {
   return (
     <div id="reservation">
       <section className="reservation-container">
         <h2>Reservation</h2>
+
+        <AddressInput />
       </section>
     </div>
   );
